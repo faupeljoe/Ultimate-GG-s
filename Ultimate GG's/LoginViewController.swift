67b799794014c07e8e741bce0d54aa1902387171
Joe-Andre <faupeljoe@gmail.com>
2021-06-01 00:39:04 -0700

    Made Login/Sign Up UI

---
+++ Ultimate GG's/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Ultimate GG's
+//
+//  Created by Joe-Andre on 6/1/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
